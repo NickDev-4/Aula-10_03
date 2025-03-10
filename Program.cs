b1ff8396780f7a4bf3a115c9963481e6dd790043
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-03-10 10:07:42 -0300

    123

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Informe seu nome: ");
+var nome = Console.ReadLine();
+
+Console.Write("\nBem vindo, ");
+Console.WriteLine(nome);
+
+// CTRL + ALT = SELECIONA VARIAS LINHAS
